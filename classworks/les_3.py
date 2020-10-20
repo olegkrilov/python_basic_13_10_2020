eebaae2cb7eddd54e9f742e0389ffa0620c5676c
Oleg Krilov <oleg.krilov@enkesystems.com>
2020-10-20 22:46:46 +0300

    classwork

---
+++ classworks/les_3.py
@@ -0,0 +1,18 @@
+"""
+написать
+map, zip, reduce, sorted, max, min, sum, range, round, divmod
+
+"""
+
+# Functions
+
+#
+# def my_map():
+#     return ''
+#
+#
+# def some_f(val, val_pow=2):
+#     return val ** val_pow
+#
+#
+# print(list(map(some_f, [(1, 2), (2, 3), (3, 0), 4, 5, 6])))
