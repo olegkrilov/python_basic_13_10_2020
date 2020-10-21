cd47dab8a47a9971a6c72611104aba13dd54e919
Oleg Krilov <oleg.krilov@enkesystems.com>
2020-10-21 14:16:26 +0300

    task 3 resolved

---
+++ homeworks/les_3/task3.py
@@ -0,0 +1,48 @@
+from functools import reduce
+
+print('\n3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,')
+print('и возвращает сумму наибольших двух аргументов.\n')
+
+
+def request_bool(message=''):
+    choice = str(input(f'{message} (y / n)? >>> ')).lower()
+    if choice in ['y', 'n']:
+        return choice == 'y'
+    else:
+        print('Type Y or N')
+        return request_bool(message)
+
+
+def request_number(message=''):
+    try:
+        return float(input(message + ' >>> '))
+    except ValueError:
+        print('ValueError: Should be a NUMBER')
+        return request_number(message)
+
+
+def my_func(*args):
+    return (
+        f'{args[0]} + {args[1]}',
+        reduce(lambda agg, val: agg + val, args[0: 2])
+    )
+
+
+def main():
+    done = False
+
+    while not done:
+        args = tuple(
+            sorted(
+                [request_number('Input ') for i in range(3)],
+                reverse=True
+            )
+        )
+        resp = my_func(*args)
+
+        print(f'\n{resp[0]} = {resp[1]}')
+
+        done = not request_bool('\nWould you like to repeat')
+
+
+main()
