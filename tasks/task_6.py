0484f2e68989b67ea0766c5ccab46e30b95bfedf
Oleg Krilov <oleg.krilov@enkesystems.com>
2020-10-26 11:11:14 +0200

    task 6 resolved

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,13 +1,75 @@
+from itertools import (
+    count,
+    cycle
+)
+from bycicles.input_requests import (
+    request_number,
+    request_from_list
+)
+from bycicles.helpers import (
+    show_data,
+    generate_seed
+)
+
+
 def show_task():
     print('\nTASK # 6')
-    print('Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника.')
-    print('В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия.')
-    print('Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.\n')
+    print('Реализовать два небольших скрипта:')
+    print('а) итератор, генерирующий целые числа, начиная с указанного,')
+    print('б) итератор, повторяющий элементы некоторого списка, определенного заранее.\n')
+
+
+def _select_generator():
+
+    def _integer_generator():
+        val = request_number('Input start point (int):')
+        print(f'Start from {val}')
+        return count(val)
+
+    def _array_repeat_script():
+        data = generate_seed(length=5)
+        print(f'\nOriginal data:\n{data}')
+        return cycle(data)
+
+    _generators = {
+        'I': {
+            'label': 'Integer Generator script',
+            'get_generator': _integer_generator
+        },
+        'R': {
+            'label': 'Predefined Array Repeat script',
+            'get_generator': _array_repeat_script
+        },
+        'Q': {
+            'label': 'Quit',
+            'get_generator': None
+        }
+    }
+    _keys = []
+    _msg = '\nSelect:\n' + ('-' * 42) + '\n'
+
+    for key, value in _generators.items():
+        _keys.append(key.lower())
+        _msg += f'[{key}]: {value["label"]}\n'
+
+    return _generators[request_from_list(
+        _keys,
+        _msg
+    ).upper()]
 
 
 def main():
+    done = False
+
     show_task()
-    print('Work in progress...')
+    while not done:
+        user_choice = _select_generator()
+
+        if callable(user_choice['get_generator']):
+            _generator = user_choice['get_generator']()
+            show_data([next(_generator) for _ in range(12)], user_choice['label'])
+        else:
+            done = True
 
 
 if __name__ == '__main__':
